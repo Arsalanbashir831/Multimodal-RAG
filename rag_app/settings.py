bccba482efe919525919df3b8851060ad3661565
arsalanbashir <arsalanbashir831@gmail.com>
2025-07-28 16:54:28 +0000

    push to production

--- rag_app/settings.py
+++ rag_app/settings.py
@@ -45,16 +45,17 @@ if not url:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.3","localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["192.168.1.3","localhost", "127.0.0.1","148.230.84.83","app.buildtechsupply.com"]
 #CSRF_TRUSTED_ORIGINS = ["http://192.168.10.9:8000"]
 
 
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.10.9:3000",   # client laptop origin (React/Vite/etc)
-    
     "http://localhost:3000",
+"https://buildtechsupply.com",
+'https://ai-doc-coral.vercel.app/'
 ]
 
 
